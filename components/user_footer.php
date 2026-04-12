fbc72c8e7a3e78ab32671658ba1f714d8cde5a32
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-04-12 20:14:14 +0300

    Розсилка + дороблений футер

--- components/user_footer.php
+++ components/user_footer.php
@@ -37,3 +37,50 @@
         <img src="image/brand4.png">
     </div>
 </div>
+<footer>
+    <div class="content">
+        <div class="box">
+            <img src="image/logo.png">
+            <p>Тут має бути щось написано про те, як зв'язатись з компанією</p>
+            <a href="contact.php" class="btn">Зв'яжіться з нами</a>
+        </div>
+        <div class="box">
+            <h3>Наш акаунт</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>Наш акаунт</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Історія замовлень</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Кошик</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Розсилка</a>
+        </div>
+        <div class="box">
+            <h3>Інформація</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>Про нас</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Зв'яжіться з нами</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Політика конфіденційність</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Умови та положення</a>
+        </div>
+        <div class="box">
+            <h3>Додаткові послуги</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>Бренди</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Подарунковий сертифікат</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Партнери</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Спеціальні пропозиції</a>
+        </div>
+        <div class="box">
+            <h3>Зв'яжіться з нами</h3>
+            <p><i class="bx bxs-phone"></i>+380986708240</p>
+            <p><i class="bx bxs-envelope"></i>anastasiya.semeniuk@gmail.com</p>
+            <p><i class="bx bxs-location-plus"></i>м.Коломия вул.Едельвейсів 495</p>
+            <div class="icons">
+                <i class="bx bxl-facebook"></i>
+                <i class="bx bxl-instagram-alt"></i>
+                <i class="bx bxl-linkedin"></i>
+                <i class="bx bxl-twitter"></i>
+                <i class="bx bxl-pinterest-alt"></i>
+            </div>
+        </div>
+    </div>
+    <div class="bottom">
+        <p>copyright @ 2026 Anastaiia Semeniuk. Всі права захищено</p>
+        <a href="admin/register.php">Адмін</a>
+    </div>
+</footer>
