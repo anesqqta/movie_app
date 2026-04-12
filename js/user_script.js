3191dc8444e5bf3c7fe7effe49868e2ef3d4c811
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-04-12 17:07:53 +0300

    Кнопка сторінок для телефону в хедері

--- js/user_script.js
+++ js/user_script.js
@@ -4,3 +4,9 @@ const userBox = document.querySelector('.profile');
 userBtn.addEventListener('click', function() {
     userBox.classList.toggle('active');
 });
+
+const toggle = document.querySelector('#menu-btn');
+toggle.addEventListener('click', function() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+})
