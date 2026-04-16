77c3752f290bb8840d3587287e252a48742d3ffb
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-04-16 17:13:52 +0300

    Банер для сторінки 'про нас'

--- home.php
+++ home.php
@@ -310,21 +310,7 @@
      </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     
     <?php include 'components/user_footer.php'; ?>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
