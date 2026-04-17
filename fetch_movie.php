996ebb3779833abd008dab3a2cbf4b0387d13de9
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-04-17 11:22:39 +0300

    Банер для сторінки 'всі фільми'

---
+++ fetch_movie.php
@@ -0,0 +1,45 @@
+<?php
+    include 'components/connect.php';
+    if (isset($_COOKIE['user_id'])){
+        $user_id = $_COOKIE['user_id'];
+    }else{
+        $user_id = '';
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name = "viewport" content="width=device-width, initial-scale=1">
+        <!-- посилання на іконки  -->
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="css/user_style.css?v=<?php echo time(); ?>">
+        <title>BOLETO</title>
+    </head>
+    <body>
+
+    <?php include 'components/user_header.php'; ?>
+
+    <div class="banner">
+        <div class="detail">
+            <h1>Всі фільми</h1>
+            <p>Написати про нас</p>
+            <span><a href="home.php">Головна</a><i class="bx bxs-right-arrow-alt"></i>всі фільми</span>
+        </div>
+    </div>
+
+
+    <?php include 'components/user_footer.php'; ?>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+        <script type="text/javascript">
+            <?php include 'js/user_script.js'; ?>
+        </script>
+
+
+        <?php include 'components/alert.php'; ?>
+    </body>
+</html>
