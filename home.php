f7f74fe66fd22b88fa70fe9f405f283c36c8f649
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-04-17 13:01:49 +0300

    Змінено, щоб розсилка була лише на головній сторінці, а не на всіх, бо підключена до футера

--- home.php
+++ home.php
@@ -309,6 +309,27 @@
         </div>
      </div>
 
+    <!-- розсилка -->
+    <div class="newletter">
+        <div class="overlay"></div>
+        <div class="box-detail">
+            <span>Підпишіться на Boleto</span>
+            <h1>щоб отримати ексклюзивні переваги</h1>
+            <p>Ми поважаємо вашу конфіденційність, тому ніколи не передаємо вашу інформацію</p>
+            <div class="input-fielt">
+                <input type="mail" name="" placeholder="введіть ваш email..." class="box"><br>
+                <button type="" class="btn">підпишись</button>
+            </div>
+            <div class="icons">
+                <i class="bx bxl-facebook"></i>
+                <i class="bx bxl-instagram-alt"></i>
+                <i class="bx bxl-linkedin"></i>
+                <i class="bx bxl-twitter"></i>
+                <i class="bx bxl-pinterest-alt"></i>
+            </div>
+        </div>
+    </div>
+
 
 
     <?php include 'components/user_footer.php'; ?>
