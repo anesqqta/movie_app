c10f8bf4c17869810a2c9dbe7bca8f913bbcfe1d
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-04-17 12:57:52 +0300

    Пошук + сторінка невдалого пошуку

---
+++ notfound.php
@@ -0,0 +1,42 @@
+<?php
+    include 'components/connect.php';
+    if (isset($_COOKIE['user_id'])){
+        $user_id = $_COOKIE['user_id'];
+    }else{
+        $user_id = '';
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name = "viewport" content="width=device-width, initial-scale=1">
+        <!-- посилання на іконки  -->
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="css/user_style.css?v=<?php echo time(); ?>">
+        <title>BOLETO</title>
+    </head>
+    <body>
+
+    <?php include 'components/user_header.php'; ?>
+
+    <div class="not-found">
+        <h1>УПСС! Не змогли знайти.</h1>
+        <img src="image/404.png"><br>
+        <a href="home.php" class="btn">Повернутися на головну сторінку</a>
+    </div>
+
+    <?php include 'components/user_footer.php'; ?>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+        <script type="text/javascript">
+            <?php include 'js/user_script.js'; ?>
+        </script>
+
+
+        <?php include 'components/alert.php'; ?>
+    </body>
+</html>
---
+++ search_movie.php
@@ -0,0 +1,99 @@
+<?php
+    include 'components/connect.php';
+    if (isset($_COOKIE['user_id'])){
+        $user_id = $_COOKIE['user_id'];
+    }else{
+        $user_id = '';
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name = "viewport" content="width=device-width, initial-scale=1">
+        <!-- посилання на іконки  -->
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="css/user_style.css?v=<?php echo time(); ?>">
+        <title>BOLETO</title>
+    </head>
+    <body>
+
+    <?php include 'components/user_header.php'; ?>
+
+    <div class="banner">
+        <div class="detail">
+            <h1>Результати пошуку</h1>
+            <p>Написати про нас</p>
+            <span><a href="home.php">Головна</a><i class="bx bxs-right-arrow-alt"></i>результати пошуку</span>
+        </div>
+    </div>
+
+    <!-- секція пошуку фільмів -->
+    <div class="show-movie">
+        <div class="heading">
+            <span>movie listings</span>
+            <h1>movies listings in boleto</h1>
+        </div>
+        <div class="box-container">
+            <?php
+                if (isset($_POST['search_movie']) or isset($_POST['search_movie_btn'])){
+                    $search_movies = $_POST['search_movie'];
+                    $select_movies = $conn->prepare("SELECT * FROM movies WHERE title LIKE '%{$search_movies}%' AND status = ?");
+                    $select_movies->execute(['active']);
+
+                    if ($select_movies->rowCount() > 0) {
+                        while($fetch_movie = $select_movies->fetch(PDO::FETCH_ASSOC)){
+
+                       
+            ?>
+            <form action="" method="post" class="box">
+                <img src="uploaded_files/<?= $fetch_movie['thumbnail'] ?>"  class="img1">
+
+                <div class="content">
+                    <div class="button">
+                        <div><h3><?= $fetch_movie['title'] ?></h3></div>
+                        <div>
+                            <button type="submit" name="add_to_wishlist"><img src="image/heart.png"></button>
+                            <a href="<?= $fetch_movie['trailer_url'] ?>" class="bx bx-play"></a>
+                            <a href="view_movie.php?pid=<?= $fetch_movie['id'] ?>" class="bx bx-show"></a>
+                        </div>
+                    </div>
+                    <div class="rate">
+                        <p><span><img src="image/tomato.png"></span>88%</p>
+                        <p><img src="image/cake.png">98%</p>
+                    </div>
+                    <input type="hidden" name="movie_id" value="<?= $fetch_movie['id']; ?>">
+                    <a href="select-language.php?movie_id=<?=$fetch_movie['id']; ?>" class="btn">бронювати квиток</a>
+                </div>
+            </form>
+            <?php
+                        }
+                    }else{
+                        header('Location: notfound.php');
+                        exit;
+                    }
+                }else{
+                    echo '
+                    <div class="empty">
+                        <p>no movie added yet!</p>
+                    </div>
+                    ';
+                }
+            ?>
+        </div>
+    </div>
+
+    <?php include 'components/user_footer.php'; ?>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+        <script type="text/javascript">
+            <?php include 'js/user_script.js'; ?>
+        </script>
+
+
+        <?php include 'components/alert.php'; ?>
+    </body>
+</html>
