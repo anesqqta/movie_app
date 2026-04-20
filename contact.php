b42e2539191c7747b7e803fc1ebae997a022e8dc
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-04-20 18:23:48 +0300

    Секція контактів (надсилання повідомлення) на сторінці 'контакти'

--- contact.php
+++ contact.php
@@ -6,6 +6,37 @@
         $user_id = '';
     }
 
+    if (isset($_POST['send_message'])) {
+        if ($user_id != '') {
+            $id = unique_id();
+
+            $name = $_POST['name'];
+            $name = filter_var($name, FILTER_SANITIZE_STRING);
+
+            $email = $_POST['email'];
+            $email = filter_var($email, FILTER_SANITIZE_STRING);
+
+            $subject = $_POST['subject'];
+            $subject = filter_var($subject, FILTER_SANITIZE_STRING);
+
+            $message = $_POST['message'];
+            $message = filter_var($message, FILTER_SANITIZE_STRING);
+
+            $verify_message = $conn->prepare("SELECT * FROM message WHERE user_id = ? AND name = ? AND email = ? AND subject = ? AND message = ?");
+            $verify_message->execute([$user_id, $name, $email, $subject, $message]);
+
+            if ($verify_message->rowCount() > 0) {
+                $warning_msg[] = 'Повідомлення вже надіслано';
+            }else{
+                $insert_message = $conn->prepare("INSERT INTO message (id, user_id, name, email, subject, message) VALUES (?, ?, ?, ?, ?, ?)");
+                $insert_message->execute([$id, $user_id, $name, $email, $subject, $message]);
+                $success_msg[] = 'Повідомлення надіслано';
+            }
+        }else{
+            $warning_msg[] = 'Увійдіть спочатку';
+        }
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -63,7 +94,39 @@
         </div>
     </div>
 
-    
+    <!-- секція контактів -->
+    <div class="contact">
+        <div class="heading">
+            <h1>Зв'яжіться</h1>
+            <p>написати щось</p>
+        </div>
+        <div class="box-container">
+            <div class="form-container">
+                <form action="" method="post" enctype="multipart/form-data" class="register">
+                    <div class="input-field">
+                        <p>Ваше ім'я <span>*</span></p>
+                        <input type="text" name="name" required maxlength="50" placeholder="Введіть ім'я" class="box">
+                    </div>
+                    <div class="input-field">
+                        <p>Ваш email <span>*</span></p>
+                        <input type="email" name="email" required maxlength="50" placeholder="Введіть email" class="box">
+                    </div>
+                    <div class="input-field">
+                        <p>Ваша тема <span>*</span></p>
+                        <input type="text" name="subject" required maxlength="50" placeholder="Введіть тему" class="box">
+                    </div>
+                    <div class="input-field">
+                        <p>Ваше повідомлення <span>*</span></p>
+                        <textarea name="message" class="box"></textarea>
+                    </div>
+                    <button type="submit" name="send_message" class="btn">Надіслати повідомлення</button>
+                </form>
+            </div>
+            <div class="box">
+                <img src="image/contact.jpg">
+            </div>
+        </div>
+    </div>
     
     
 
