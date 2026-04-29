4a0ef55653c1d239e17407c34f89545ae02da533
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-04-29 11:15:30 +0300

    Сторінка реєстрації для адміна

--- admin/register.php
+++ admin/register.php
@@ -1,11 +1,6 @@
 <?php
-    include 'components/connect.php';
-    if (isset($_COOKIE['user_id'])){
-        $user_id = $_COOKIE['user_id'];
-    }else{
-        $user_id = '';
-    }
-
+    include '../components/connect.php';
+    
 ?>
 
 <!DOCTYPE html>
@@ -15,11 +10,44 @@
         <meta name = "viewport" content="width=device-width, initial-scale=1">
         <!-- посилання на іконки  -->
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-        <link rel="stylesheet" type="text/css" href="css/admin_style.css?v=<?php echo time(); ?>">
+        <link rel="stylesheet" type="text/css" href="../css/admin_style.css?v=<?php echo time(); ?>">
         <title>BOLETO</title>
     </head>
     <body>
+        <div class="form-container form-area">
+            <form action="" method="post" enctype="multipart/form-data" class="register">
+                <h3>Створити акаунт</h3>
+                <div class="flex">
+                    <div class="col">
+                        <div class="input-field">
+                            <p>Ваше ім'я <span>*</span></p>
+                            <input type="text" name="name" required maxlength="50" placeholder="Введіть ім'я" class="box">
+                        </div>
+                        <div class="input-field">
+                            <p>Ваш email <span>*</span></p>
+                            <input type="email" name="email" required maxlength="50" placeholder="Введіть email" class="box">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="input-field">
+                            <p>Ваш пароль <span>*</span></p>
+                            <input type="password" name="pass" required maxlength="20" placeholder="Введіть пароль" class="box">
+                        </div>
+                        <div class="input-field">
+                            <p>Підтвердіть ваш пароль <span>*</span></p>
+                            <input type="password" name="cpass" required maxlength="20" placeholder="Введіть повторно пароль" class="box">
+                        </div>
+                    </div>
+                </div>
+                <div class="input-field">
+                    <p>Фото профілю <span>*</span></p>
+                    <input type="file" name="image" required accept="image/*" class="box">
+                </div>
+                <p class="link">Вже є обліковий запис ? <a href="login.php">Увійти</a></p>
+                <button type="submit" name="register" class="btn">зареєструватися</button>
+            </form>
+        </div>
 
-        <?php include 'components/alert.php'; ?>
+        <?php include '../components/alert.php'; ?>
     </body>
 </html>
