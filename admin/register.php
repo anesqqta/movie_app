4eab9bd12ebd35da85af283e77044ec72083a767
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-04-29 11:03:06 +0300

    Глобальні налаштування, палітра кольорів, смуга прокрутки для адміна

---
+++ admin/register.php
@@ -0,0 +1,25 @@
+<?php
+    include 'components/connect.php';
+    if (isset($_COOKIE['user_id'])){
+        $user_id = $_COOKIE['user_id'];
+    }else{
+        $user_id = '';
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name = "viewport" content="width=device-width, initial-scale=1">
+        <!-- посилання на іконки  -->
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="css/admin_style.css?v=<?php echo time(); ?>">
+        <title>BOLETO</title>
+    </head>
+    <body>
+
+        <?php include 'components/alert.php'; ?>
+    </body>
+</html>
