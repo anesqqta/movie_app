46848b5bedc37ef04ae8edbb15208e15f49c70fe
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-04-30 10:26:49 +0300

    Сторінка панелі керування адміна

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -32,16 +32,89 @@
     </div>
 
     <!-- секція панель керування-->
+    <div class="dashboard">
+        <div class="heading">
+            <span>Моя панель керування</span>
+            <h1>Панель керування</h1>
+        </div>
+        <div class="box-container">
+            <div class="box">
+                <?php
+                    $select_profile = $conn->prepare("SELECT * FROM admin WHERE id = ?");
+                    $select_profile->execute([$admin_id]);
+                    $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+                ?>
+                <h3>Вітаємо</h3>
+                <p><?= $fetch_profile['name']; ?></p>
+                <a href="update.php" class="btn">Оновити профіль</a>
+            </div>
+            <div class="box">
+                <?php
+                    $total_income = $conn->query("SELECT SUM(amount) FROM booking")->fetchColumn();
+                    if (!$total_income) {
+                        $total_income = 0;
+                    }
+                ?>
+                <h3>$ <?php echo number_format($total_income); ?></h3>
+                <p>Загальний дохід</p>
+                <div class="btn">
+                    <i class="bx bx-dollar"></i>Гривень
+                </div>
+            </div>
+            <div class="box">
+                <?php
+                    $select_users = $conn->prepare("SELECT * FROM users");
+                    $select_users->execute();
+                    $num_of_users = $select_users->rowCount();
+                ?>
+                <h3><?= $num_of_users ?></h3>
+                <p>Зареєстровано користувачів</p>
+                <a href="user_account.php" class="btn">Переглянути користувачів</a>
+            </div>
+            <div class="box">
+                <?php
+                    $select_bookings = $conn->prepare("SELECT * FROM booking");
+                    $select_bookings->execute();
+                    $total_bookings = $select_bookings->rowCount();
+                ?>
+                <h3><?= $total_bookings ?></h3>
+                <p>Загальна кількість бронювань</p>
+                <a href="admin_bookings.php" class="btn">Переглянути бронювання</a>
+            </div>
+            
+            <div class="box">
+                <?php
+                    $select_movies = $conn->prepare("SELECT * FROM movies");
+                    $select_movies->execute();
+                    $total_movies = $select_movies->rowCount();
+                ?>
+                <h3><?= $total_movies ?></h3>
+                <p>Загальна кількість фільмів</p>
+                <a href="view_movies.php" class="btn">Переглянути фільми</a>
+            </div>
+            <div class="box">
+                <?php
+                    $select_reviews = $conn->prepare("SELECT * FROM reviews");
+                    $select_reviews->execute();
+                    $total_reviews = $select_reviews->rowCount();
+                ?>
+                <h3><?= $total_reviews ?></h3>
+                <p>Загальна кількість відгуків</p>
+                <a href="comments.php" class="btn">Переглянути відгуки</a>
+            </div>
+        </div>
+    </div>
+
     
 
             
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
-        <script type="text/javascript">
-            <?php include '../js/admin_script.js'; ?>
-        </script>
+    <script type="text/javascript">
+        <?php include '../js/admin_script.js'; ?>
+    </script>
 
-        <?php include '../components/alert.php'; ?>
+    <?php include '../components/alert.php'; ?>
     </body>
 </html>
