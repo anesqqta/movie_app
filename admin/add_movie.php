95e1da9e499afcc0f6337e5cf172546856263d8c
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-04-30 13:55:23 +0300

    Функціональна сторінка додавання фільмів

--- admin/add_movie.php
+++ admin/add_movie.php
@@ -7,6 +7,104 @@
         header('location:login.php');
     }
 
+    if (isset($_POST['uploade'])) {
+        $movie_id = unique_id();
+
+        $name = $_POST['title'];
+        $name = filter_var($name, FILTER_SANITIZE_STRING);
+
+        $language = $_POST['language'];
+        $language = filter_var($language, FILTER_SANITIZE_STRING);
+
+        $genre = $_POST['genre'];
+        $genre = filter_var($genre, FILTER_SANITIZE_STRING);
+
+        $content = $_POST['content'];
+        $content = filter_var($content, FILTER_SANITIZE_STRING);
+
+        $duration = $_POST['duration'];
+        $duration = filter_var($duration, FILTER_SANITIZE_STRING);
+
+        $actors = $_POST['actors'] ?? [];
+        $actors1 = implode(',', $actors);
+
+        $crews = $_POST['crew'] ?? [];
+        $crews1 = implode(',', $crews);
+
+        $trailer_url = $_POST['trailer_url'];
+        $trailer_url = filter_var($trailer_url, FILTER_SANITIZE_STRING);
+
+        $release_year = $_POST['release_year'];
+        $release_year = filter_var($release_year, FILTER_SANITIZE_STRING);
+
+        $status = 'active';
+
+        //вставити заставку
+        $totalFiles = count($_FILES['thumbnails']['name']);
+        $fileArray = array();
+
+        for ($i=0; $i < $totalFiles; $i++) {
+            $imageName = $_FILES['thumbnails']['name'][$i];
+            $tmpName = $_FILES['thumbnails']['tmp_name'][$i];
+            $imageExtension = explode('.', $imageName);
+            $imageExtension = strtolower(end($imageExtension));
+
+            $NewImageName = unique_id().'.'.$imageExtension;
+
+            $image_folder = '../uploaded_files/'.$NewImageName;
+
+            move_uploaded_file($tmpName, $image_folder);
+
+            $fileArray[] = $NewImageName;
+        }
+        $fileArray = json_encode($fileArray);
+
+        $image = $_FILES['image']['name'];
+        $image = filter_var($image, FILTER_SANITIZE_STRING);
+        $image_size = $_FILES['image']['size'];
+        $image_tmp_name = $_FILES['image']['tmp_name'];
+        $image_folder = '../uploaded_files/'.$image;
+
+        $thumbnail = $_FILES['thumbnail']['name'];
+        $thumbnail = filter_var($thumbnail, FILTER_SANITIZE_STRING);
+        $thumbnail_size = $_FILES['thumbnail']['size'];
+        $thumbnail_tmp_name = $_FILES['thumbnail']['tmp_name'];
+        $thumbnail_folder = '../uploaded_files/'.$thumbnail;
+
+        $select_image = $conn->prepare("SELECT * FROM movies WHERE poster = ?");
+        $select_image->execute([$image]);
+
+        if (isset($image)) {
+            if ($select_image->rowCount() > 0) {
+                $warning_msg[] = 'Назва зображення повторюється';
+            }elseif ($image_size > 2000000) {
+                $warning_msg[] = 'Розмір зображення занадто великий';
+            }else{
+                move_uploaded_file($image_tmp_name, $image_folder);
+            }
+        }else{
+            $image = "";
+        }
+        if ($select_image->rowCount() > 0 AND $image != '') {
+            $warning_msg[] = 'Перейменуйте зображення';
+        }else{
+            $insert_movie = $conn->prepare("INSERT INTO movies (id, title, description, language, genre, release_year, duration, poster, thumbnail, trailer_url, movie_thumb, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $insert_movie->execute([$movie_id, $name, $content, $language, $genre, $release_year, $duration, $image, $thumbnail, $trailer_url, $fileArray, $status]);
+
+            move_uploaded_file($thumbnail_tmp_name, $thumbnail_folder);
+        }
+
+        //додавання акторського складу
+        $movie_actors = $conn->prepare("INSERT INTO movie_actors (movie_id, actor_id) VALUES (?, ?)");
+        $movie_actors->execute([$movie_id, $actors1]);
+
+        //додавання команди
+        $movie_crews = $conn->prepare("INSERT INTO movie_directors (movie_id, director_id) VALUES (?, ?)");
+        $movie_crews->execute([$movie_id, $crews1]);
+
+        $success_msg[] = 'Фільм додано!';
+    }
+
 ?>
 
 <!DOCTYPE html>
