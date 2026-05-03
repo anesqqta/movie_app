659fea7c1db4136ac93b17e5fb33f80e52833967
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-05-04 00:14:04 +0300

    Кнопка видалення сеансу

--- admin/view_show.php
+++ admin/view_show.php
@@ -6,6 +6,23 @@
         $admin_id = '';
         header('location:login.php');
     }
+
+    //видалення сеансу з бази даниих
+    if (isset($_POST['delete'])) {
+        $delete_id = $_POST['show_id'];
+        $delete_id = filter_var($delete_id, FILTER_SANITIZE_STRING);
+
+        $verify_delete = $conn->prepare("SELECT * FROM shows WHERE id = ?");
+        $verify_delete->execute([$delete_id]);
+
+        if ($verify_delete->rowCount() > 0) {
+            $delete_show = $conn->prepare("DELETE FROM shows WHERE id = ?");
+            $delete_show->execute([$delete_id]);
+            $success_msg[] = 'Сеанс видалено';
+        }else{
+            $warning_msg[] = 'Сеанс уже видалено';
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -36,6 +53,57 @@
             <h1>Деталі сеансу</h1>
             <a href="add_show.php" class="btn">+</a>
         </div> 
+        <div class="box-container">
+            <?php
+                $select_shows = $conn->prepare("SELECT * FROM shows");
+                $select_shows->execute();
+
+                if ($select_shows->rowCount() > 0) {
+                    while($fetch_show = $select_shows->fetch(PDO::FETCH_ASSOC)) {
+                        $movie_id = $fetch_show['movie_id'];
+
+                        //отримати фільм
+                        $select_movie = $conn->prepare("SELECT title FROM movies WHERE id = ?");
+                        $select_movie->execute([$movie_id]);
+                        $movie_title = $select_movie->fetchColumn();
+
+                        //отримати назву залу
+                        $hall_id = $fetch_show['hall_id'];
+                        $select_hall = $conn->prepare("SELECT name FROM halls WHERE id = ?");
+                        $select_hall->execute([$hall_id]);
+                        $hall_title = $select_hall->fetchColumn();
+            ?>
+            <table cellspacing="0">
+                <tr>
+                    <th>Назва</th>
+                    <th>Зал</th>
+                    <th>Кількість місць</th>
+                    <th>Дія</th>
+                </tr>
+                <tr>
+                    <td><?= $movie_title; ?></td>
+                    <td><?= $hall_title; ?></td>
+                    <td><?= $fetch_show['seat_no']; ?></td>
+                    <td>
+                        <form action="" method="post">
+                            <input type="hidden" name="show_id" value="<?= $fetch_show['id']; ?>">
+                            <a href="view_show.php?get_id<?= $fetch_show['id']; ?>" class="btn">Редагувати</a>
+                            <button type="submit" name="delete" onclick="return confirm('Видалити цей сеанс?');" class="btn">Видалити</button>
+                        </form>
+                    </td>
+                </tr>
+            </table>
+            <?php 
+                    }
+                }else{
+                    echo '
+                    <div class="empty">
+                        <p>Сеанс ще не додано!<br><a href="add_show.php" class="btn">Додати сеанс</a></p>
+                    </div>
+                    ';
+                }
+            ?>
+        </div>
     </div>
 
     
