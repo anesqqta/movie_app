8fb42dc19ee5175eb5eb47901d7e7fb18dda89fc
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-05-04 01:12:05 +0300

    Редагування сеансів

--- admin/view_show.php
+++ admin/view_show.php
@@ -23,6 +23,34 @@
             $warning_msg[] = 'Сеанс уже видалено';
         }
     }
+
+    //редагування 
+    if (isset($_POST['edit_show'])) {
+        $update_id = $_POST['update_id'];
+        $update_id = filter_var($update_id, FILTER_SANITIZE_STRING);
+
+        $movie_id = $_POST['movie_id'];
+        $movie_id = filter_var($movie_id, FILTER_SANITIZE_STRING);
+        
+        $hall_id = $_POST['hall_id'];
+        $hall_id = filter_var($hall_id, FILTER_SANITIZE_STRING);
+
+        $show_date = $_POST['show_date'];
+        $show_date = filter_var($show_date, FILTER_SANITIZE_STRING);
+
+        $show_time = $_POST['show_time'];
+        $show_time = filter_var($show_time, FILTER_SANITIZE_STRING);
+
+        $seat_no = $_POST['seat_no'];
+        $seat_no = filter_var($seat_no, FILTER_SANITIZE_STRING);
+
+        $update_show = $conn->prepare("UPDATE shows SET movie_id = ?, hall_id = ?, show_date = ?, show_time = ?, seat_no = ? WHERE id = ?");
+        $update_show->execute([$movie_id, $hall_id, $show_date, $show_time, $seat_no, $update_id]);
+
+        $success_msg[] = 'Сеанс оновлено!';
+        
+        header('location:view_show.php');
+    }
 ?>
 
 <!DOCTYPE html>
@@ -87,7 +115,7 @@
                     <td>
                         <form action="" method="post">
                             <input type="hidden" name="show_id" value="<?= $fetch_show['id']; ?>">
-                            <a href="view_show.php?get_id<?= $fetch_show['id']; ?>" class="btn">Редагувати</a>
+                            <a href="view_show.php?get_id=<?= $fetch_show['id']; ?>" class="btn">Редагувати</a>
                             <button type="submit" name="delete" onclick="return confirm('Видалити цей сеанс?');" class="btn">Видалити</button>
                         </form>
                     </td>
@@ -106,6 +134,109 @@
         </div>
     </div>
 
+    <div class="update-container">
+        <?php
+            if (isset($_GET['get_id'])) {
+                $edit_id = $_GET['get_id'];
+                $edit_query = $conn->prepare("SELECT * FROM shows WHERE id = ?");
+                $edit_query->execute([$edit_id]);
+
+                if ($edit_query->rowCount() > 0) {
+                    while($fetch_edit = $edit_query->fetch(PDO::FETCH_ASSOC)) {
+                        //отримати фільм
+                        $movie_id = $fetch_edit['movie_id'];
+                        $select_movie = $conn->prepare("SELECT title FROM movies WHERE id = ?");
+                        $select_movie->execute([$movie_id]);
+                        $movie_title = $select_movie->fetchColumn();
+
+                        //отримати назву залу
+                        $hall_id = $fetch_edit['hall_id'];
+                        $select_hall = $conn->prepare("SELECT name FROM halls WHERE id = ?");
+                        $select_hall->execute([$hall_id]);
+                        $hall_title = $select_hall->fetchColumn();
+
+                        //отримання часу сеансу
+                        $show_time_id = $fetch_edit['show_time'];
+                        $select_time = $conn->prepare("SELECT time FROM show_time WHERE id = ?");
+                        $select_time->execute([$show_time_id]);
+                        $show_time = $select_time->fetchColumn();
+        ?>
+        <div class="form-container">
+            <form action="" method="post" enctype="multipart/form-data" class="login">
+                <input type="hidden" name="update_id" value="<?= $fetch_edit['id']; ?>">
+                <h3>Редагувати сеанс</h3>
+                <div class="input-field">
+                    <p>Фільм <span>*</span></p>
+                    <select name="movie_id" class="box" required>
+                        <option selected disabled value="<?= $movie_title; ?>"><?= $movie_title; ?></option>
+                        <?php
+                            $select_movie = $conn->prepare("SELECT * FROM movies WHERE status = ?");
+                            $select_movie->execute(['active']);
+
+                            if ($select_movie->rowCount() > 0) {
+                                while($fetch_movie = $select_movie->fetch(PDO::FETCH_ASSOC)) {
+                        ?>
+                        <option value="<?= $fetch_movie['id']; ?>" style="background: #ccc"><?= $fetch_movie['title']; ?></option>
+                        <?php                 
+                                }
+                            }
+                        ?>
+                    </select>
+                </div>
+                <div class="input-field">
+                    <p>Зал <span>*</span></p>
+                    <select name="hall_id" class="box" required>
+                        <option selected disabled value="<?= $hall_title; ?>"><?= $hall_title; ?></option>
+                        <?php
+                            $select_hall = $conn->prepare("SELECT * FROM halls WHERE status = ?");
+                            $select_hall->execute(['active']);
+
+                            if ($select_hall->rowCount() > 0) {
+                                while($fetch_hall = $select_hall->fetch(PDO::FETCH_ASSOC)) {
+                        ?>
+                        <option value="<?= $fetch_hall['id']; ?>" style="background: #ccc"><?= $fetch_hall['name']; ?></option>
+                        <?php                 
+                                }
+                            }
+                        ?>
+                    </select>
+                </div>
+                <div class="input-field">
+                    <p>Дата сеансу <span>*</span></p>
+                    <input type="date" name="show_date" maxlength="50" value="<?= $fetch_edit['show_date']; ?>" min="<?php echo date('Y-m-d') ?>" class="box">
+                </div>
+                <div class="input-field">
+                    <p>Час сеансу <span>*</span></p>
+                    <select name="show_time" class="box" required>
+                        <option selected disabled value="<?= $show_time; ?>">Виберіть час сеансу</option>
+                        <?php
+                            $select_show_time = $conn->prepare("SELECT * FROM show_time");
+                            $select_show_time->execute();
+
+                            if ($select_show_time->rowCount() > 0) {
+                                while($fetch_show_time = $select_show_time->fetch(PDO::FETCH_ASSOC)) {
+                        ?>
+                        <option value="<?= $fetch_show_time['id']; ?>" style="background: #ccc"><?= $fetch_show_time['time']; ?></option>
+                        <?php                 
+                                }
+                            }
+                        ?>
+                    </select>
+                </div>
+                <div class="input-field">
+                    <p>Кількість доступних місць <span>*</span></p>
+                    <input type="text" name="seat_no" maxlength="50" value="<?= $fetch_edit['seat_no']; ?>" class="box">
+                </div>
+                <button type="submit" name="edit_show" class="btn">Редагувати сеанс</button>
+            </form>
+        </div>
+        <?php 
+                    }
+                }
+                echo "<script>document.querySelector('.update-container').style.display='block'</script>";
+            }
+        ?>
+    </div>
     
     
     
